4b62957d853a3241384e3902e9c35334459c8a4e
Manu Mtz-Almeida <manu.valladolid@gmail.com>
2014-07-02 01:50:33 +0200

    Do not update header when status code is less than 1

--- gin.go
+++ gin.go
@@ -344,7 +344,9 @@ func (c *Context) HTML(code int, name string, data interface{}) {
 
 // Writes the given string into the response body and sets the Content-Type to "text/plain"
 func (c *Context) String(code int, msg string) {
-	c.Writer.WriteHeader(code)
+	if code >= 0 {
+		c.Writer.WriteHeader(code)
+	}
 	c.Writer.Header().Set("Content-Type", "text/plain")
 	c.Writer.Write([]byte(msg))
 }
