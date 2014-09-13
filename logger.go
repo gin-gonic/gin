b4a6510edff7d580da9e0d1aed85352969d546d5
Manu Mtz-Almeida <manu.valladolid@gmail.com>
2014-09-13 15:51:29 +0200

    Enables colored tag for http method in gin.Logger()

--- logger.go
+++ logger.go
@@ -92,12 +92,12 @@ func Logger() HandlerFunc {
 		}
 		end := time.Now()
 		latency := end.Sub(start)
-		stdlogger.Printf("[GIN] %v |%s %3d %s| %12v | %s |%s %-7s %s| %s\n%s",
+		stdlogger.Printf("[GIN] %v |%s %3d %s| %12v | %s |%s  %s %-7s %s\n%s",
 			end.Format("2006/01/02 - 15:04:05"),
 			color, code, reset,
 			latency,
 			requester,
-			methodColor, method, reset,
+			methodColor, reset, method,
 			c.Request.URL.Path,
 			c.Errors.String(),
 		)
