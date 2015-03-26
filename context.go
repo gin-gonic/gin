31323f694b1f1bb14992040c851a838c02e0be74
Manu Mtz-Almeida <manu.valladolid@gmail.com>
2015-03-26 04:27:34 +0100

    AbortIndex is missing

--- context.go
+++ context.go
@@ -57,6 +57,7 @@ func (a errorMsgs) String() string {
 	}
 	return buffer.String()
 }
+const AbortIndex = math.MaxInt8 / 2
 
 // Context is the most important part of gin. It allows us to pass variables between middleware,
 // manage the flow, validate the JSON of a request and render a JSON response for example.
