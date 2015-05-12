59c836e1fadb3d830be38281aa0e69467adf8c01
Manu Mtz-Almeida <manu.valladolid@gmail.com>
2015-05-12 18:31:31 +0200

    Only emit a warning is the status code changes

--- response_writer.go
+++ response_writer.go
@@ -42,11 +42,11 @@ func (w *responseWriter) reset(writer http.ResponseWriter) {
 }
 
 func (w *responseWriter) WriteHeader(code int) {
-	if code > 0 {
-		w.status = code
+	if code > 0 && w.status != code {
 		if w.Written() {
-			debugPrint("[WARNING] Headers were already written")
+			debugPrint("[WARNING] Headers were already written. Wanted to override status code %d with %d", w.status, code)
 		}
+		w.status = code
 	}
 }
 
