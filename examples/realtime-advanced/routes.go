05d01d2282a7cf1d78bd4525ee6e0637be4eec35
Manu Mtz-Almeida <manu.valladolid@gmail.com>
2015-05-14 22:52:37 +0200

    Restored old limits

--- examples/realtime-advanced/routes.go
+++ examples/realtime-advanced/routes.go
@@ -14,11 +14,11 @@ func rateLimit(c *gin.Context) {
 
 	ip := c.ClientIP()
 	value := int(ips.Add(ip, 1))
-	if value%10 == 0 {
+	if value%50 == 0 {
 		fmt.Printf("ip: %s, count: %d\n", ip, value)
 	}
-	if value >= 100 {
-		if value%100 == 0 {
+	if value >= 200 {
+		if value%200 == 0 {
 			fmt.Println("ip blocked")
 		}
 		c.Abort()
