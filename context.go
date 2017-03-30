7189e9ba8b79f57f101208737f6b3ed3cacae6a7
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-03-30 15:32:17 +0800

    feat: add GetRawData func.

--- context.go
+++ context.go
@@ -7,6 +7,7 @@ package gin
 import (
 	"errors"
 	"io"
+	"io/ioutil"
 	"math"
 	"mime/multipart"
 	"net"
@@ -443,6 +444,12 @@ func (c *Context) Header(key, value string) {
 // GetHeader returns value from request headers
 func (c *Context) GetHeader(key string) string {
 	return c.requestHeader(key)
+}
+
+// GetRawData return stream data
+func (c *Context) GetRawData() (body []byte, err error) {
+	body, err = ioutil.ReadAll(c.Request.Body)
+	return
 }
 
 func (c *Context) SetCookie(
