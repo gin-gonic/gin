f8520b83f912848dea56595ca7ec2f652a72b4ee
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-04-17 13:41:47 +0800

    update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ examples/auto-tls/example1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping handler
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	log.Fatal(autotls.Run(r, "example1.com", "example2.com"))
+}
