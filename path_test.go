3b8150c83c5304be4dd6b49f212cda6fe3aeed10
Javier Provecho Fernandez <javiertitan@gmail.com>
2017-06-27 22:54:42 +0200

    Revert "unify license (#942)"
    This reverts commit 53799774f45a119234a89f791565e53aa26b4482.

--- path_test.go
+++ path_test.go
@@ -1,6 +1,7 @@
-// Copyright 2017 Manu Martinez-Almeida.  All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
+// Copyright 2013 Julien Schmidt. All rights reserved.
+// Based on the path package, Copyright 2009 The Go Authors.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
 
 package gin
 
