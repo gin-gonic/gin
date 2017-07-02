92ddc7d2405b441d5fc9b64e938628dec1382759
Javier Provecho Fernandez <javiertitan@gmail.com>
2017-07-02 17:06:53 +0200

    docs(test): add missing inline docs

--- test_helpers.go
+++ test_helpers.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 )
 
+// CreateTestContext returns a fresh engine and context for testing purposes
 func CreateTestContext(w http.ResponseWriter) (c *Context, r *Engine) {
 	r = New()
 	c = r.allocateContext()
