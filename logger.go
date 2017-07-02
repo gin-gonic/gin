484acfc30396fa215376f9c2454d1bdf14c86c29
Javier Provecho Fernandez <javiertitan@gmail.com>
2017-07-02 17:05:21 +0200

    docs(logger): add missing inline docs

--- logger.go
+++ logger.go
@@ -25,14 +25,17 @@ var (
 	disableColor = false
 )
 
+// DisableConsoleColor disables color output in the console
 func DisableConsoleColor() {
 	disableColor = true
 }
 
+// ErrorLogger returns a handlerfunc for any error type
 func ErrorLogger() HandlerFunc {
 	return ErrorLoggerT(ErrorTypeAny)
 }
 
+// ErrorLoggerT returns a handlerfunc for a given error type
 func ErrorLoggerT(typ ErrorType) HandlerFunc {
 	return func(c *Context) {
 		c.Next()
