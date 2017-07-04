df67c644795eaa050b378df62129eb08bd98d0ff
田欧 <thinkerou@gmail.com>
2017-07-04 10:08:41 +0800

    fix cleanPath spell (#969)

--- path.go
+++ path.go
@@ -5,7 +5,7 @@
 
 package gin
 
-// CleanPath is the URL version of path.Clean, it returns a canonical URL path
+// cleanPath is the URL version of path.Clean, it returns a canonical URL path
 // for p, eliminating . and .. elements.
 //
 // The following rules are applied iteratively until no further processing can
