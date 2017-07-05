56793b0e157bd5337994e9cf7eff97c75bac434e
thinkerou <thinkerou@gmail.com>
2017-07-05 17:27:27 +0800

    update comment method

--- auth.go
+++ auth.go
@@ -89,6 +89,6 @@ func secureCompare(given, actual string) bool {
 	if subtle.ConstantTimeEq(int32(len(given)), int32(len(actual))) == 1 {
 		return subtle.ConstantTimeCompare([]byte(given), []byte(actual)) == 1
 	}
-	/* Securely compare actual to itself to keep constant time, but always return false */
+	// Securely compare actual to itself to keep constant time, but always return false
 	return subtle.ConstantTimeCompare([]byte(actual), []byte(actual)) == 1 && false
 }
