352fd4915b4d4f9c98baaf0583f0a2b59d9f26ab
Eason Lin <easonlin404@gmail.com>
2017-07-07 01:12:49 +0800

    Use single line

--- utils.go
+++ utils.go
@@ -100,12 +100,10 @@ func parseAccept(acceptHeader string) []string {
 	parts := strings.Split(acceptHeader, ",")
 	out := make([]string, 0, len(parts))
 	for _, part := range parts {
-		index := strings.IndexByte(part, ';')
-		if index >= 0 {
+		if index := strings.IndexByte(part, ';'); index >= 0 {
 			part = part[0:index]
 		}
-		part = strings.TrimSpace(part)
-		if len(part) > 0 {
+		if part = strings.TrimSpace(part); len(part) > 0 {
 			out = append(out, part)
 		}
 	}
