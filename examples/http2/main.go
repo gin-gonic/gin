87fdff8f88e22722ce258ebb46e5285341ebe523
田欧 <thinkerou@gmail.com>
2017-07-11 09:03:09 +0800

    [docs] add http2 example (#1000)

---
+++ examples/http2/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+
+	"github.com/gin-gonic/gin"
+)
+
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>Https Test</title>
+</head>
+<body>
+  <h1 style="color:red;">Welcome, Ginner!</h1>
+</body>
+</html>
+`))
+
+func main() {
+	r := gin.Default()
+	r.SetHTMLTemplate(html)
+
+	r.GET("/welcome", func(c *gin.Context) {
+		c.HTML(200, "https", gin.H{
+			"status": "success",
+		})
+	})
+
+	// Listen and Server in https://127.0.0.1:8080
+	r.RunTLS(":8080", "./testdata/server.pem", "./testdata/server.key")
+}
