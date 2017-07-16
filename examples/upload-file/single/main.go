93b3a0d7ec95c33dc327397ab1756c36853328ee
Eason Lin <easonlin404@gmail.com>
2017-07-16 11:42:08 +0800

    feat(context): add SaveUploadedFile func. (#1022)
    * feat(context): add SaveUploadedFile func.
    
    * feat(context): update multiple upload examples.
    
    * style(example): fix gofmt
    
    * fix(example): add missing return

--- examples/upload-file/single/main.go
+++ examples/upload-file/single/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,23 +20,11 @@ func main() {
 			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
 			return
 		}
-		src, err := file.Open()
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("file open err: %s", err.Error()))
+
+		if err := c.SaveUploadedFile(file, file.Filename); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
 			return
 		}
-		defer src.Close()
-
-		// Destination
-		dst, err := os.Create(file.Filename)
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("Create file err: %s", err.Error()))
-			return
-		}
-		defer dst.Close()
-
-		// Copy
-		io.Copy(dst, src)
 
 		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email))
 	})
