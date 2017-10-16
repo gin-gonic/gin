4afad2518921d22d63d77a7a272ca42ff406459d
Jithin James <jithinjamesgec@gmail.com>
2017-10-16 16:25:56 +0530

    Print all handlers on start-up

--- doc.go
+++ doc.go
@@ -3,4 +3,4 @@ Package gin implements a HTTP web framework called gin.
 
 See https://gin-gonic.github.io/gin/ for more information about gin.
 */
-package gin // import "github.com/gin-gonic/gin"
+package gin // import "github.com/jithinjk/gin"
