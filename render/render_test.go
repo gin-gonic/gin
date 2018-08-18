b6cb77e3155544ea0b90b621af52516ae2b6f192
salamer <salamer_gaga@163.com>
2018-08-18 11:42:27 +0800

    add full protobuf rendering test

--- render/render_test.go
+++ render/render_test.go
@@ -15,6 +15,8 @@ import (
 	"strings"
 	"testing"
 
+	testdata "github.com/gin-gonic/gin/testdata/protoexample"
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/ugorji/go/codec"
 )
@@ -252,9 +254,6 @@ b:
 	assert.Equal(t, "application/x-yaml; charset=utf-8", w.Header().Get("Content-Type"))
 }
 
-func TestRenderProtoBuf(t *testing.T) {
-}
-
 type fail struct{}
 
 // Hook MarshalYAML
@@ -266,6 +265,25 @@ func TestRenderYAMLFail(t *testing.T) {
 	w := httptest.NewRecorder()
 	err := (YAML{&fail{}}).Render(w)
 	assert.Error(t, err)
+}
+
+// test Protobuf rendering
+func TestRenderProtoBuf(t *testing.T) {
+	w := httptest.NewRecorder()
+	reps := []int64{int64(1), int64(2)}
+	label := "test"
+	data := &testdata.Test{
+		Label: &label,
+		Reps:  reps,
+	}
+
+	(ProtoBuf{data}).WriteContentType(w)
+	protoData, err := proto.Marshal(data)
+	assert.NoError(t, err)
+	(YAML{data}).WriteContentType(w)
+	assert.Equal(t, "application/x-yaml; charset=utf-8", w.Header().Get("Content-Type"))
+
+	assert.Equal(t, string(protoData[:]), w.Body.String())
 }
 
 func TestRenderXML(t *testing.T) {
