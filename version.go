397615a052e63f51de297c6882770edc9230afff
thinkerou <thinkerou@gmail.com>
2018-09-15 13:29:34 +0800

    chore: add a version file includes gin version

---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright 2018 Gin Core Team.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package gin
+
+// Version is the current gin framework's version.
+const Version = "v1.4.0"
