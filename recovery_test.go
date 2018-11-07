f5542958adae6e3281fea674030a3f9c5856f3c2
thinkerou <thinkerou@gmail.com>
2018-11-07 18:59:59 +0800

    remove build tag

--- recovery_test.go
+++ recovery_test.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-// +build go1.7
-
 package gin
 
 import (
--- response_writer_1.7.go
+++ response_writer_1.7.go
@@ -1,8 +1,8 @@
-// +build !go1.8
-
 // Copyright 2018 Gin Core Team.  All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
+
+// +build !go1.8
 
 package gin
 
