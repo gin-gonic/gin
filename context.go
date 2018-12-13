f67d7a90c4d2e5bdf310a78d7e6a04e3d9aee851
Romain Beuque <rbeuque74@gmail.com>
2018-12-13 12:20:17 +0900

    context: inherits context cancelation and deadline from http.Request context for Go>=1.7 (#1690)
    *gin.Context implements standard context.Context methods, but always
    returns data as context is still valid. Since Go 1.7, http.Request now
    contains a context.Context object, which can be controlled by the
    http.Server to indicates that the context is now closed, and persue of
    request should be canceled.
    
    This implements the propagation of http.Request context methods inside
    gin.Context to have HTTP context cancelation information at gin.Context
    level.
    
    Signed-off-by: Romain Beuque <romain.beuque@corp.ovh.com>

--- context.go
+++ context.go
@@ -942,34 +942,6 @@ func (c *Context) SetAccepted(formats ...string) {
 	c.Accepted = formats
 }
 
-/************************************/
-/***** GOLANG.ORG/X/NET/CONTEXT *****/
-/************************************/
-
-// Deadline returns the time when work done on behalf of this context
-// should be canceled. Deadline returns ok==false when no deadline is
-// set. Successive calls to Deadline return the same results.
-func (c *Context) Deadline() (deadline time.Time, ok bool) {
-	return
-}
-
-// Done returns a channel that's closed when work done on behalf of this
-// context should be canceled. Done may return nil if this context can
-// never be canceled. Successive calls to Done return the same value.
-func (c *Context) Done() <-chan struct{} {
-	return nil
-}
-
-// Err returns a non-nil error value after Done is closed,
-// successive calls to Err return the same error.
-// If Done is not yet closed, Err returns nil.
-// If Done is closed, Err returns a non-nil error explaining why:
-// Canceled if the context was canceled
-// or DeadlineExceeded if the context's deadline passed.
-func (c *Context) Err() error {
-	return nil
-}
-
 // Value returns the value associated with this context for key, or nil
 // if no value is associated with key. Successive calls to Value with
 // the same key returns the same result.
