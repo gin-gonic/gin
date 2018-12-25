2d33c82028b4085827137c5a72cc0a076d8e2b08
Sai <sairoutine@gmail.com>
2018-12-26 00:27:24 +0900

    Add comment to LogFormatterParams struct's fields (#1711)
    By https://github.com/gin-gonic/gin/issues/1701, I thought it's necessary.

--- logger.go
+++ logger.go
@@ -45,15 +45,24 @@ type LogFormatter func(params LogFormatterParams) string
 
 // LogFormatterParams is the structure any formatter will be handed when time to log comes
 type LogFormatterParams struct {
-	Request      *http.Request
-	TimeStamp    time.Time
-	StatusCode   int
-	Latency      time.Duration
-	ClientIP     string
-	Method       string
-	Path         string
+	Request *http.Request
+
+	// TimeStamp shows the time after the server returns a response.
+	TimeStamp time.Time
+	// StatusCode is HTTP response code.
+	StatusCode int
+	// Latency is how much time the server cost to process a certain request.
+	Latency time.Duration
+	// ClientIP equals Context's ClientIP method.
+	ClientIP string
+	// Method is the HTTP method given to the request.
+	Method string
+	// Path is a path the client requests.
+	Path string
+	// ErrorMessage is set if error has occurred in processing the request.
 	ErrorMessage string
-	IsTerm       bool
+	// IsTerm shows whether does gin's output descriptor refers to a terminal.
+	IsTerm bool
 }
 
 // defaultLogFormatter is the default log format function Logger middleware uses.
