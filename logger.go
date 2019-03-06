1be9ccae8e5d1a7e82b90fe401c495d620495b7e
Adam Zielinski <zielinskia@wit.edu>
2019-03-05 22:55:50 -0500

    pass context Keys to LogFormatterParams

--- logger.go
+++ logger.go
@@ -66,8 +66,8 @@ type LogFormatterParams struct {
 	IsTerm bool
 	// BodySize is the size of the Response Body
 	BodySize int
-	// Context is the Context associated with the request.
-	Context *Context
+	// Keys are the keys set on the request's context.
+	Keys map[string]interface{}
 }
 
 // StatusCodeColor is the ANSI color for appropriately logging http status code to a terminal.
@@ -229,7 +229,7 @@ func LoggerWithConfig(conf LoggerConfig) HandlerFunc {
 			param := LogFormatterParams{
 				Request: c.Request,
 				IsTerm:  isTerm,
-				Context: c,
+				Keys:    c.Keys,
 			}
 
 			// Stop timer
