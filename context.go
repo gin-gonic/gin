e0ca0b16ab99c7a44dcbfade85bc9d588d6dceb7
eeonevision <d7vladislav@gmail.com>
2019-11-25 11:35:11 +0300

    Added custom http status code to context's render

--- context.go
+++ context.go
@@ -39,6 +39,8 @@ const (
 
 const abortIndex int8 = math.MaxInt8 / 2
 
+const httpStatusClientClosedRequest = 499
+
 // Context is the most important part of gin. It allows us to pass variables between middleware,
 // manage the flow, validate the JSON of a request and render a JSON response for example.
 type Context struct {
