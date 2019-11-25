d71693964f6e129b67b90fa9e928cc687a5aa655
fifsky <fifsky@gmail.com>
2019-11-25 11:50:09 +0800

    fix fmt

--- binding/validate_test.go
+++ binding/validate_test.go
@@ -9,8 +9,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/assert"
 )
 
 type testInterface interface {
