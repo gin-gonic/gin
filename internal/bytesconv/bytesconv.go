982daeb1ecdced87bbef6c11783a640eb88a193a
Andy Pan <panjf2000@gmail.com>
2020-01-18 00:32:50 +0800

    Use zero-copy approach to convert types between string and byte… (#2206)
    * Use zero-copy approach to convert types between string and byte slice
    
    * Rename argument to a eligible one
    
    Benchmark:
    
    BenchmarkBytesConvBytesToStrRaw-4   	21003800	        70.9 ns/op	      96 B/op	       1 allocs/op
    BenchmarkBytesConvBytesToStr-4      	1000000000	         0.333 ns/op	       0 B/op	       0 allocs/op
    BenchmarkBytesConvStrToBytesRaw-4   	18478059	        59.3 ns/op	      96 B/op	       1 allocs/op
    BenchmarkBytesConvStrToBytes-4      	1000000000	         0.373 ns/op	       0 B/op	       0 allocs/op
    
    
    Co-authored-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ internal/bytesconv/bytesconv.go
@@ -0,0 +1,19 @@
+package bytesconv
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// StringToBytes converts string to byte slice without a memory allocation.
+func StringToBytes(s string) (b []byte) {
+	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	bh.Data, bh.Len, bh.Cap = sh.Data, sh.Len, sh.Len
+	return b
+}
+
+// BytesToString converts byte slice to string without a memory allocation.
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
