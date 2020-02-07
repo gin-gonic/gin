6fa61ce75cc819fee460d7647b4fa82a44657142
Kaushik Neelichetty <kaushikn@thoughtworks.com>
2020-02-07 17:07:42 +0530

    update JSON renderer to use Marshall() instead of Encode(). Fix #2209

--- render/json.go
+++ render/json.go
@@ -69,8 +69,11 @@ func (r JSON) WriteContentType(w http.ResponseWriter) {
 // WriteJSON marshals the given interface object and writes it with custom ContentType.
 func WriteJSON(w http.ResponseWriter, obj interface{}) error {
 	writeContentType(w, jsonContentType)
-	encoder := json.NewEncoder(w)
-	err := encoder.Encode(&obj)
+	jsonBytes, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(jsonBytes)
 	return err
 }
 
