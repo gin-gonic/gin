22a881fbd65e123b9d480a7f6821fc0f2209fef8
Manuel Alonso <manuelalonso@invisionapp.com>
2020-03-07 14:51:33 +0100

    removing log injection (#2277)
    Co-authored-by: thinkerou <thinkerou@gmail.com>

--- logger.go
+++ logger.go
@@ -141,7 +141,7 @@ var defaultLogFormatter = func(param LogFormatterParams) string {
 		// Truncate in a golang < 1.8 safe way
 		param.Latency = param.Latency - param.Latency%time.Second
 	}
-	return fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %s\n%s",
+	return fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
 		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
 		statusColor, param.StatusCode, resetColor,
 		param.Latency,
