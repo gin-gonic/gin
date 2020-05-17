e3336ad2f558794eee256f1d2d6b6d7e13cf7c4b
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-05-17 12:50:11 +0800

    chore: update

--- tree.go
+++ tree.go
@@ -41,12 +41,6 @@ func (ps Params) ByName(name string) (va string) {
 	return
 }
 
-func strToBytes(s string) []byte {
-	x := (*[2]uintptr)(unsafe.Pointer(&s))
-	h := [3]uintptr{x[0], x[1], x[1]}
-	return *(*[]byte)(unsafe.Pointer(&h))
-}
-
 func bytesToStr(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
