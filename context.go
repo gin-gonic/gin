d6d43c4e05025958ed63b9d2523b10ecbfdb6cd7
deepoil <run-to-run@i.softbank.jp>
2020-08-06 15:57:59 +0900

    delete unused const MINEXML2

--- context.go
+++ context.go
@@ -29,7 +29,6 @@ const (
 	MIMEJSON              = binding.MIMEJSON
 	MIMEHTML              = binding.MIMEHTML
 	MIMEXML               = binding.MIMEXML
-	MIMEXML2              = binding.MIMEXML2
 	MIMEPlain             = binding.MIMEPlain
 	MIMEPOSTForm          = binding.MIMEPOSTForm
 	MIMEMultipartPOSTForm = binding.MIMEMultipartPOSTForm
