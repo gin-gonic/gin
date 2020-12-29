1a0ef67aa8c75b18b8dfd2cc30f18263fc9dd36d
Jeff <laojianzi1994@gmail.com>
2020-12-29 14:28:10 +0800

    test: fixed the TestUnixSocket test on windows (#20)

--- gin_integration_test.go
+++ gin_integration_test.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"sync"
 	"testing"
 	"time"
@@ -146,7 +147,7 @@ func TestRunWithPort(t *testing.T) {
 func TestUnixSocket(t *testing.T) {
 	router := New()
 
-	unixTestSocket := "/tmp/unix_unit_test"
+	unixTestSocket := filepath.Join(os.TempDir(), "unix_unit_test")
 
 	defer os.Remove(unixTestSocket)
 
