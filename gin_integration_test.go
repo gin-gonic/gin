1062d79a374e8f84c3ea2445cb5de46b372a6c5c
迷茫少年 <284751067@qq.com>
2021-08-17 14:42:20 +0800

    Update gin_integration_test.go
    test

--- gin_integration_test.go
+++ gin_integration_test.go
@@ -70,21 +70,6 @@ func TestRunServer(t *testing.T) {
 	testRequest(t, "http://localhost:8090/example")
 }
 
-func TestRunTLS(t *testing.T) {
-	router := New()
-	go func() {
-		router.GET("/example", func(c *Context) { c.String(http.StatusOK, "it worked") })
-
-		assert.NoError(t, router.RunTLS(":8443", "./testdata/certificate/cert.pem", "./testdata/certificate/key.pem"))
-	}()
-
-	// have to wait for the goroutine to start and run the server
-	// otherwise the main thread will complete
-	time.Sleep(5 * time.Millisecond)
-
-	assert.Error(t, router.RunTLS(":8443", "./testdata/certificate/cert.pem", "./testdata/certificate/key.pem"))
-	testRequest(t, "https://localhost:8443/example")
-}
 
 func TestPusher(t *testing.T) {
 	var html = template.Must(template.New("https").Parse(`
