ba0ada880ea62ab06cf9be38bb01599c3b59bb8c
jincheng9 <perfume0607@gmail.com>
2021-12-02 17:03:43 +0800

    fix: grammar error

--- logger.go
+++ logger.go
@@ -178,7 +178,7 @@ func ErrorLoggerT(typ ErrorType) HandlerFunc {
 }
 
 // Logger instances a Logger middleware that will write the logs to gin.DefaultWriter.
-// By default gin.DefaultWriter = os.Stdout.
+// By default, gin.DefaultWriter = os.Stdout.
 func Logger() HandlerFunc {
 	return LoggerWithConfig(LoggerConfig{})
 }
