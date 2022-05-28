4b68a5f12af4d6d2be83e1895f783d5dd5d5a148
thinkerou <thinkerou@gmail.com>
2022-05-28 10:42:28 +0800

    chore: update go.mod and remove space from copyright (#3158)
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- errors.go
+++ errors.go
@@ -1,4 +1,4 @@
-// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
+// Copyright 2014 Manu Martinez-Almeida. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
