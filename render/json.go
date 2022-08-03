31bc4c08cd12dda70313531f307c23526371b1b0
TimAndy <xuchonglei@126.com>
2022-08-03 19:40:04 +0800

    Export json codec

--- render/json.go
+++ render/json.go
@@ -10,8 +10,8 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/gin-gonic/gin/codec/json"
 	"github.com/gin-gonic/gin/internal/bytesconv"
-	"github.com/gin-gonic/gin/internal/json"
 )
 
 // JSON contains the given interface object.
