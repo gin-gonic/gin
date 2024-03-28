6c8ec4deda45b39eb12b9d24e6ef43ab4b7d827f
cyal1 <33282478+cyal1@users.noreply.github.com>
2024-03-28 23:05:09 +0800

    fixed open redirect

--- gin.go
+++ gin.go
@@ -699,6 +699,7 @@ func redirectTrailingSlash(c *Context) {
 		p = prefix + "/" + req.URL.Path
 	}
 	req.URL.Path = p + "/"
+	p = regRemoveRepeatedChar.ReplaceAllString(p, "/")
 	if length := len(p); length > 1 && p[length-1] == '/' {
 		req.URL.Path = p[:length-1]
 	}
