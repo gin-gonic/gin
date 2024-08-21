6c7a318468ce708b91c1afbf4a32e493d65fab22
jimchen <jimchen@easyops.cn>
2024-08-21 09:45:05 +0800

    fix: fix URL path value still unescaped when UnescapePathValues set false

--- gin.go
+++ gin.go
@@ -645,10 +645,10 @@ func (engine *Engine) HandleContext(c *Context) {
 
 func (engine *Engine) handleHTTPRequest(c *Context) {
 	httpMethod := c.Request.Method
-	rPath := c.Request.URL.EscapedPath()
+	rPath := c.Request.URL.Path
 	unescape := false
-	if engine.UseRawPath && len(c.Request.URL.RawPath) > 0 {
-		rPath = c.Request.URL.RawPath
+	if engine.UseRawPath {
+		rPath = c.Request.URL.EscapedPath()
 		unescape = engine.UnescapePathValues
 	}
 
