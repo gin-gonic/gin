eaaa030a66798b013b02d3e1315ab93e92e37b60
TimAndy <xuchonglei@126.com>
2024-12-31 09:57:58 +0800

    refactor(json): export json codec

--- binding/json.go
+++ binding/json.go
@@ -10,7 +10,7 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/gin-gonic/gin/internal/json"
+	"github.com/gin-gonic/gin/codec/json"
 )
 
 // EnableDecoderUseNumber is used to call the UseNumber method on the JSON
