1822e6338e7e00de67f132003589f7975e8f5336
voyager1 <voyager1@voyager1deMacBook-Pro.local>
2025-03-18 15:21:25 +0800

    docs: fix case error of X-Real-IP

--- context.go
+++ context.go
@@ -889,7 +889,7 @@ func (c *Context) ShouldBindBodyWithPlain(obj any) error {
 
 // ClientIP implements one best effort algorithm to return the real client IP.
 // It calls c.RemoteIP() under the hood, to check if the remote IP is a trusted proxy or not.
-// If it is it will then try to parse the headers defined in Engine.RemoteIPHeaders (defaulting to [X-Forwarded-For, X-Real-Ip]).
+// If it is it will then try to parse the headers defined in Engine.RemoteIPHeaders (defaulting to [X-Forwarded-For, X-Real-IP]).
 // If the headers are not syntactically valid OR the remote IP does not correspond to a trusted proxy,
 // the remote IP (coming from Request.RemoteAddr) is returned.
 func (c *Context) ClientIP() string {
