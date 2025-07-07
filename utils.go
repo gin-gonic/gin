e8622bf445ffb81bf4bced0ecfeca2712710a02b
keitosuwahara <keitosuwahara0816@gmail.com>
2025-07-07 19:09:46 +0900

    refactor function joinPaths and isAscii

--- utils.go
+++ utils.go
@@ -114,13 +114,6 @@ func parseAccept(acceptHeader string) []string {
 	return out
 }
 
-func lastChar(str string) uint8 {
-	if str == "" {
-		panic("The length of the string can't be 0")
-	}
-	return str[len(str)-1]
-}
-
 func nameOfFunction(f any) string {
 	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
 }
@@ -131,7 +124,7 @@ func joinPaths(absolutePath, relativePath string) string {
 	}
 
 	finalPath := path.Join(absolutePath, relativePath)
-	if lastChar(relativePath) == '/' && lastChar(finalPath) != '/' {
+	if strings.HasSuffix(relativePath, "/") && !strings.HasSuffix(finalPath, "/") {
 		return finalPath + "/"
 	}
 	return finalPath
@@ -155,10 +148,7 @@ func resolveAddress(addr []string) string {
 
 // https://stackoverflow.com/questions/53069040/checking-a-string-contains-only-ascii-characters
 func isASCII(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if s[i] > unicode.MaxASCII {
-			return false
-		}
-	}
-	return true
+	return !strings.ContainsFunc(s, func(r rune) bool {
+		return r > unicode.MaxASCII
+	})
 }
